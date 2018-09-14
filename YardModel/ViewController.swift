769023ab1e0ce38605dd17ad4fc66c534fdcc69e
Татьяна Гончарова <tanja211030@yandex.ru>
2018-09-14 23:18:42 +0100

    HW2

--- YardModel/ViewController.swift
+++ YardModel/ViewController.swift
@@ -23,11 +23,73 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
         
+        loadYard()
+        trees()
+        
+    }
+    
+    func loadYard()  {
         // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        let scene = SCNScene(named: "art.scnassets/yard.scn")!
         
         // Set the scene to the view
         sceneView.scene = scene
+
+    }
+    
+    func mainNode() -> SCNNode {
+        for node in sceneView.scene.rootNode.childNodes {
+            if node.name == "yard" {
+                return node
+            }
+        }
+        return SCNNode()
+    }
+    
+    func getTree(positin: SCNVector3) -> SCNNode {
+        let nodeTree = SCNNode()
+        nodeTree.position = positin
+        
+        let nodeTrunk = SCNNode()
+        nodeTrunk.position = SCNVector3(0, 0.75, 0)
+        let trunk = SCNCylinder(radius: 0.05, height: 1.5)
+        trunk.firstMaterial?.diffuse.contents = UIColor.brown
+        trunk.firstMaterial?.diffuse.contents = UIImage(named: "art.scnassets/trunk1.jpg")
+        trunk.firstMaterial?.lightingModel = SCNMaterial.LightingModel.physicallyBased
+
+        nodeTrunk.geometry = trunk
+        
+        let nodeCrown = SCNNode()
+        nodeCrown.position = SCNVector3(0, 1.5, 0)
+        nodeCrown.scale = SCNVector3(1, 2, 1)
+        let crown = SCNSphere(radius: 0.3)
+        crown.firstMaterial?.diffuse.contents = UIImage(named: "art.scnassets/crown1.jpg")
+        crown.firstMaterial?.lightingModel = SCNMaterial.LightingModel.physicallyBased
+        nodeCrown.geometry = crown
+        
+        nodeTree.addChildNode(nodeCrown)
+        nodeTree.addChildNode(nodeTrunk)
+
+        return nodeTree
+    }
+    
+    func trees() {
+        let mainNode = self.mainNode()
+        
+        mainNode.addChildNode(getTree(positin: SCNVector3(-4.5, 0, 4.5)))
+        mainNode.addChildNode(getTree(positin: SCNVector3(-4.5, 0, -4.5)))
+        mainNode.addChildNode(getTree(positin: SCNVector3(4.5, 0, -4.5)))
+        mainNode.addChildNode(getTree(positin: SCNVector3(4.5, 0, 4.5)))
+        
+        mainNode.addChildNode(getTree(positin: SCNVector3(-4.5, 0, -1.5)))
+        mainNode.addChildNode(getTree(positin: SCNVector3(-4.5, 0, 1.5)))
+
+        mainNode.addChildNode(getTree(positin: SCNVector3(4.5, 0, -1.5)))
+        mainNode.addChildNode(getTree(positin: SCNVector3(4.5, 0, 1.5)))
+
+        mainNode.addChildNode(getTree(positin: SCNVector3(-1.5, 0, -4.5)))
+        mainNode.addChildNode(getTree(positin: SCNVector3(1.5, 0, -4.5)))
+
     }
     
     override func viewWillAppear(_ animated: Bool) {
